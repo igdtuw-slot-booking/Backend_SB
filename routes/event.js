037c7301adc804f7536ee30cbfa6ca5f4636e8a6
igdtuw-slot-booking <112639584+igdtuw-slot-booking@users.noreply.github.com>
2023-01-15 20:45:52 +0530

    Update event.js

--- routes/event.js
+++ routes/event.js
@@ -1,29 +1,32 @@
 import express from "express";
-import { createEvent , updateEvent , deleteEvent , getSingleEvent , getallEvent, countByStatus, myEvents } from "../controllers/event.js";
-import { verifyUser, verifyAdmin } from "../utils/verifyToken.js";         //use verifyadmin verifyuser from verifyToken.js as per the requirement ---like kis function ke liye kya verification needed hai
+import { createEvent , updateEvent , deleteEvent , getSingleEvent , getallEvent, countByStatus, myEvents, updateEventByUser } from "../controllers/event.js";
+import { verifyUser, verifyAdmin, isAuthenticatedUser, authorizeRoles } from "../utils/verifyToken.js";         //use verifyadmin verifyuser from verifyToken.js as per the requirement ---like kis function ke liye kya verification needed hai
 
 const router = express.Router();
 
 //CREATE
-router.post("/", verifyUser, createEvent);
+router.post("/", isAuthenticatedUser, createEvent);
 
 //UPADATE EVENT STATUS --by admin
-router.put("/:id", verifyAdmin, updateEvent);
+router.put("/:id", isAuthenticatedUser, authorizeRoles("admin"), updateEvent);
 
 //DECLINE EVENT --by admin
 //router.delete("/:id", verifyAdmin, deleteEvent );
 
 //GET LOGGED IN USER EVENTS
-router.get("/find/me", verifyUser, myEvents);
+router.get("/find/me", isAuthenticatedUser, myEvents);
 
 //GET
-router.get("/find/:id", verifyUser, verifyAdmin, getSingleEvent);
+router.get("/find/:id", isAuthenticatedUser, authorizeRoles("admin"), getSingleEvent);
 
 
 //GETALL
-router.get("/", verifyAdmin, getallEvent);
+router.get("/", isAuthenticatedUser, authorizeRoles("admin"), getallEvent);
 
 router.get("/countByStatus", countByStatus);
+
+//UPDATE EVENT STATUS TO CANCEL --by user
+router.put("/userCancel/:id", isAuthenticatedUser, updateEventByUser);
 
 
 

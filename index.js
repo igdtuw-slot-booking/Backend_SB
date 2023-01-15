bcafa034bc8e7b1c36157036e2152f48adb79e5f
igdtuw-slot-booking <112639584+igdtuw-slot-booking@users.noreply.github.com>
2023-01-15 21:08:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ import cors from "cors";
 const app = express();
 dotenv.config();
 
+mongoose.set("strictQuery", false);
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO)

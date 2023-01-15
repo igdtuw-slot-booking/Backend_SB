4f5d067650e03b50b734be63eb50f7c8caa5796e
igdtuw-slot-booking <112639584+igdtuw-slot-booking@users.noreply.github.com>
2023-01-15 18:20:09 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -28,22 +28,44 @@ export const register = async (req,res,next)=>{
 
 export const login = async (req,res,next)=>{
     try{
-        const user = await User.findOne({email: req.body.email});
-        if(!user) return next(createError(404, "User not found!"));
+        const { email, password } = req.body;
 
-        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
-        if(!isPasswordCorrect) 
-            return next(createError(400, "Wrong username or password!"));
-        
-        const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT);
-        
-        
-        const { password, isAdmin, ...otherDetails } = user._doc;
-        res.cookie("access_token", token, {
-            httpOnly: true,
-        }).status(200).json({ user:{password, isAdmin, ...otherDetails}});
-    }catch(err){
-        next(err);
+    const user = await User.findOne({ email })
+      .select("+password");
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect password",
+      });
+    }
+
+    const token = await user.generateToken();
+
+    const options = {
+      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    };
+
+    res.status(200).cookie("token", token, options).json({
+      success: true,
+      user,
+      token,
+    });
+    }catch(error){
+      res.status(500).json({
+      success: false,
+      message: error.message,
+      });
     }
 };
 
